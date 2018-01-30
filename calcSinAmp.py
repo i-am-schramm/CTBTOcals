f62a527f6deb0a402cd4ce59a05b05837e535c2a
Schramm <kschramm@usgs.gov>
2018-01-30 15:48:53 -0700

    Initializing repo

---
+++ calcSinAmp.py
@@ -0,0 +1,22 @@
+#!/bin/env python
+
+''' given a start time and filename this script will read in a waveform 
+and return the amplitude'''
+
+# the start time should be the beginning of the actual sine wave, not 
+# the beginning of the cal input. I will investigate how the time in 
+#the seed blockette matches up for this.
+
+from obspy import read, UTCDateTime
+
+#fileName = '/tr1/telemetry_days/IU_SFJD/2018/2018_018/10_BH2.512.seed'
+fileName = '/msd/IU_SFJD/2015/029/_BC1.512.seed'
+#calStart=UTCDateTime("2018,018,00,33,00")
+calStart=UTCDateTime("2015,029,05,20,00")
+print(calStart)
+
+st = read(fileName,starttime=calStart, endtime=calStart+600)
+st.plot()
+sinAmp=st[0].std()
+
+print(sinAmp)
