8a8d4ff6bd2ab942adf610b0709f86da41ce7e5f
Schramm <kschramm@usgs.gov>
2018-02-09 16:08:29 -0700

    reading in inv but not working quite as expected

--- getPAZinfo.py
+++ getPAZinfo.py
@@ -2,10 +2,34 @@
 
 ''' fetch the resp paz for full-frequency response email '''
 
+from obspy.io.xseed import Parser
+from obspy import UTCDateTime
+
 station='SFJD'
 network='IU'
 channel='BHZ'
 location='10'
 
-:wq
+# this has more than one volume index control header found and doesn't work
+#respFile="/dcc/metadata/dataless/DATALESS."+network+"_"+station+".seed"
 
+# this seems to work, but then you have to search for station.
+# it will also give warnings about more than one abbreviation control header found...
+respFile='/APPS/metadata/SEED/'+network+'.dataless'
+
+#create and inventory of the dataless resp info
+respIn=Parser(respFile)
+inv = respIn.get_inventory()
+nslc=network+'.'+station+'.'+location+'.'+channel
+
+for ch in inv['channels']:
+    if(ch['channel_id']==nslc):
+      print(ch['channel_id'])
+      print(ch.get_paz)
+
+
+####
+'''Traceback (most recent call last):
+  File "getPAZinfo.py", line 28, in <module>
+    print(ch.get_paz)
+AttributeError: 'dict' object has no attribute 'get_paz''''
